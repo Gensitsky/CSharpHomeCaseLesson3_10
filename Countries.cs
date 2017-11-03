bfca5b8af709227a5c6353756ec5e0dbbaaf2ac0
Vadim Gensitsky <Vadim Gensitsky>
2017-11-03 11:57:00 +0200

    Add project files.

---
+++ Countries.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Ukraine
+{
+    class Kiev
+    {
+        public static int Population()
+        {
+            int population = 2804000;
+            return population;
+        }
+    }
+}
+
+namespace Russia
+{
+    class Moscow
+    {
+        public static int Population()
+        {
+            int population = 11920000;
+            return population;
+        }
+    }
+}
+
+namespace Belarus
+{
+    class Minsk
+    {
+        public static int Population()
+        {
+            int population = 1911000;
+            return population;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+    Разработать приложение, в котором бы сравнивалось
+    население трёх столиц из разных стран. Причём стра-
+    на бы обозначалась пространством имён, а город —
+    классом в данном пространстве.
+*/
+using System;
+using System.Linq;
+using Ukraine;
+using Russia;
+using Belarus;
+
+namespace HomeCaseCS_Lesson3_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Cyties = new int[]
+            {
+                Ukraine.Kiev.Population(),
+                Russia.Moscow.Population(),
+                Belarus.Minsk.Population()
+            };
+
+            Console.WriteLine($"The largest city has: " +
+                $"{Cyties.Max()} population");
+            Console.WriteLine($"The smallest city has: " +
+                $"{Cyties.Min()} population");
+
+            Console.ReadKey();
+        }
+    }
+}
